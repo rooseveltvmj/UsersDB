32e0e421a77b1e0afdb44b387e7faa0b29c712c3
Roosevelt <roosevelt.vmj@gmail.com>
2026-08-26 20:00:20 -0300

    Adicionado nas requisiçoes o try e o catch

--- server.js
+++ server.js
@@ -18,10 +18,98 @@ const pool = mysql.createPool({
     queueLimit: 0
 });
 
+
+//BUSCA OS USUARIOS
 app.get("/users", async (req, res) => {
-    const [rows] = await pool.query("SELECT * FROM users");
-    res.json({"result": rows});
+    try{
+        const [rows] = await pool.query("SELECT * FROM users");
+        return res.json({"result": rows});
+    }
+    catch(error){
+        console.error("Erro, tente novamente :(", error);
+        return res.status(500).json({"msg": "Algo deu errado!"});
+    }
 });
+
+
+//CADASTRO DE USUARIOS
+app.post("/users", async (req, res) => {
+    try {
+        const {nome, sexo} = req.body;
+        // let nome = req.body;
+        // // let sexo = req.body;
+
+        /*if(nome == "senai") {
+            throw new Error("Nome invalido");
+         } */
+
+        if (!nome || !sexo) {
+            return res.status(400).json ({"msg": "Nome e sexo são obrigatorios."});
+        }
+
+        const [result] = await pool.query(
+            "INSERT INTO users VALUES(null, ?, ?);",
+            [nome, sexo]
+        );
+
+        return res.status(201).json({
+            msg: "Deu certo."
+        });
+
+        }
+        catch (error) {
+            console.error("Erro ao cadastrar usuário", error);
+            return res.status(500).json({"msg": "Algo deu errado ao cadastrar o usuario" });
+        }
+});
+
+
+//DELETAR OS USUARIOS - PASSAR O VALOR POR PARAMETROS
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const [result] = await pool.query
+        ("DELETE FROM users WHERE id = ?;",
+        [id]
+    );
+    
+    if (result === -1) {
+        return res.status(404).json({ result: "Usuário não encontrado!" });
+    }
+    
+    return res.status(200).json({ result: "Usuário removido com sucesso" });
+}
+    catch (error) {
+        console.error("Erro ao deletar usuário:", error);
+        return res.status(500).json({ "msg": "Algo deu errado ao remover o usuário!" });
+    }
+});
+
+
+//ATUALIZA OS USUARIOS - PASSAR O VALOR POR PARAMETROS E BODY
+app.patch('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        let nome = req.body;
+        let sexo = req.body;
+    
+        const [result] = await pool.query(
+            "UPDATE users SET nome = ?, sexo = ? WHERE id = ?;",
+            [nome, sexo, id]
+            );
+            
+            if (result === 0) {
+                return res.status(404).json({ result: "Usuário não encontrado!" });
+            }
+            
+            return res.status(200).json({ result: "Sucesso ao atualizar!" });
+        }
+        catch (error) {
+            console.error("Erro ao atualizar usuário:", error);
+            return res.status(500).json({ "msg": "Algo deu errado ao atualizar o usuário!" });
+    }
+});
+
 
 app.listen(port, () => {
     console.log("Servidor na porta: " + port);
